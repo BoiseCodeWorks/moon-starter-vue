50bbd69b8028333f7e829aed4c0c37fbb33c1137
Jake Overall <joverall22@gmail.com>
2020-03-18 19:45:29 -0600

    moon starter

---
+++ src/components/Moon.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :style="{'background-color':color}">
+    <img src="../assets/logo.png" height="70" alt @click="collect" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Moon",
+  props: {
+    color: { type: String, default: "green" }
+  },
+  methods: {
+    collect() {
+      console.log("COLLECTING");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="text-center">Moon Miner</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col text-center">
+          <Moon color="orange" />
+        </div>
+        <div class="col text-center">
+          <Moon color="purple" />
+        </div>
+        <div class="col text-center">
+          <Moon color="chartreuse" />
+        </div>
+        <div class="col text-center">
+          <Moon />
+        </div>
+      </div>
+      <button :disabled="cheese < 15" @click="buyIdleUpgrade">Buy Rover</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      idleCount: 0,
+      cheese: 15
+    };
+  },
+  methods: {
+    buyIdleUpgrade() {
+      if (this.idleCount == 0) {
+        this.startIdleMiner();
+        this.idleCount++;
+      }
+    },
+    startIdleMiner() {
+      setInterval(this.idleMine, 3000);
+    },
+    idleMine() {
+      this.$store.dispatch("idleMiner");
+    }
   }
-}
+};
 </script>
+
+<style>
+.home {
+  height: 100vh;
+  width: 100vw;
+  background-image: url(../assets/logo.png);
+  background-size: contain;
+}
+</style>
